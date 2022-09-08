37bd32bdd5f53d60d9a40b5b2bf514e21a6df9a6
gabriel bauer <gabriel.bauer9@hotmail.com>
2022-09-08 13:48:47 -0300

    feat: user context

---
+++ contexts/AuthContext.tsx
@@ -0,0 +1,58 @@
+import Router from "next/router";
+import { createContext, ReactNode, useState } from "react";
+import { api } from '../services/api';
+
+type User = {
+  email: string;
+  permission: string[];
+  roles: string[];
+}
+
+type SignInCredentials = {
+  email: string;
+  password: string;
+}
+
+type AuthContextData = {
+  signIn(credentials: SignInCredentials): Promise<void>;
+  user: User;
+  isAuthenticated: boolean;
+}
+
+type AuthProviderProps = {
+  children: ReactNode;
+}
+
+export const AuthContext = createContext({} as AuthContextData)
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<User>({} as User);
+  const isAuthenticated = !!user;
+
+  async function signIn({ email, password }: SignInCredentials) {
+    try {
+      const response = await api.post('sessions', {
+        email,
+        password
+      })
+
+      const { token, refreshToken, permission, roles } = response.data;
+  
+      setUser({
+        email,
+        permission,
+        roles
+      })
+
+      Router.push('/dashboard')
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return (
+    <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
