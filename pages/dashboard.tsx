7f6a4fbbf17cbeb9e1fd9ccce9c0c0e9d9c04cf7
Gabriel Bauer <gabriel.bauer9@hotmail.com>
2022-09-14 22:27:27 -0300

    feat: logout broadcast

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,15 +1,12 @@
-import { destroyCookie } from "nookies";
 import { useContext, useEffect } from "react"
 import { Can } from "../components/Can";
 import { AuthContext } from "../contexts/AuthContext"
-import { useCan } from "../hooks/useCan";
 import { setupApiClient } from "../services/api";
 import { api } from "../services/apiClient";
-import { AuthTokenError } from "../services/errors/AuthTokenError";
 import { withSSRAuth } from "../utils/withSSRAuth";
 
 export default function Dashboard() {
-  const { user } = useContext(AuthContext);
+  const { user, signOut } = useContext(AuthContext);
 
   useEffect(() => {
     api.get('/me')
@@ -23,6 +20,8 @@ export default function Dashboard() {
     <>
       <h1>Dashboard: {user?.email}</h1>
 
+      <button onClick={signOut}>SignOut</button>
+
       <Can permissions={['metrics.list']}>
         <div>métricas</div>
       </Can>
@@ -31,11 +30,6 @@ export default function Dashboard() {
 }
 
 export const getServerSideProps = withSSRAuth(async (ctx) => {
-  const apiClient = setupApiClient(ctx);
-  const response = await apiClient.get('/me');
-
-  console.log(response);
-  
   return {
     props: {}
   }
